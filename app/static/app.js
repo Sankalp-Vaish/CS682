48b410f649017a478c8c6c69a988a02e424aaa79
Prarthana-Prathap <Prarthana.Prathap001@umb.edu>
2023-04-06 00:27:46 -0400

    Adding address search for Map

--- app/static/app.js
+++ app/static/app.js
@@ -56,3 +56,51 @@ async function setLocation() {
 
   marker.setMap(map);
 }
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40.749933, lng: -73.98633 },
+    zoom: 13,
+    mapTypeControl: false,
+  });
+  const input = document.getElementById("pac-input");
+  const options = {
+    fields: ["formatted_address", "geometry", "name"],
+    strictBounds: false,
+    types: ["address"],
+  };
+  const autocomplete = new google.maps.places.Autocomplete(input, options);
+
+  const marker = new google.maps.Marker({
+    map,
+    anchorPoint: new google.maps.Point(0, -29),
+  });
+
+  autocomplete.addListener("place_changed", () => {
+    marker.setVisible(false);
+
+    const place = autocomplete.getPlace();
+
+    if (!place.geometry || !place.geometry.location) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+    // infowindowContent.children["place-name"].textContent = place.name;
+    // infowindowContent.children["place-address"].textContent = place.formatted_address;
+    // infowindow.open(map, marker);
+    marker.setMap(map);
+  });
+}
